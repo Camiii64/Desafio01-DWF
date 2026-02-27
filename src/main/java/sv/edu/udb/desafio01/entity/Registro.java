b5b9aac953ec1a2800dbcdf88fb80a7b46eb7e59
Camila Beltrán <camibeltranmedrano@gmail.com>
2026-02-26 20:52:28 -0600

    Configuración del proyecto

---
+++ src/main/java/sv/edu/udb/desafio01/entity/Registro.java
@@ -0,0 +1,22 @@
+package sv.edu.udb.desafio01.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "registros")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Registro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String descripcion;
+
+}
